bb5be0473d2cb4ac025e3dfe3b8c6c0ac8273bae
Zuzana Dostalova <zuzana.dostalova@merck.com>
2016-02-18 20:07:13 +0100

    refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
+require 'rest_client'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  require 'rest_client'
   before_action :authenticate
 
   def authenticate
     # Authorization already exists
     if authorization = Authorization.find_by(token: params["token"])
       @current_user = authorization.user
-    # Authorization doesnt exist yet - either a new user or existing one with expired token
+      # Authorization doesnt exist yet - either a new user or existing one with expired token
     else
       user_info = get_user_info
       if user_info
@@ -20,20 +21,21 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def current_user
+    @current_user
+  end
+
   private
 
   # if the token is valid, it returns user data
   def get_user_info
-    begin
-      response = RestClient.get 'https://iapi.merck.com/oauth2/v1/userinfo',
-                                      { 'Authorization' => 'Bearer ' + params["token"] }
-    rescue RestClient::Unauthorized => e
-      puts 'You are so unauthorized :O'
-      puts e.inspect
-    end
-
-    if response.try(:code) == 200 
+    response = RestClient.get 'https://iapi.merck.com/oauth2/v1/userinfo',
+      { 'Authorization' => 'Bearer ' + params["token"] }
+    if response.code == 200
       JSON.parse(response)
     end
+  rescue RestClient::Unauthorized => e
+    puts 'You are so unauthorized :O'
+    puts e.inspect
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
 
   def index
     events = Event.all
-    if @current_user
+    if current_user
       render json: events
     else
       render json: {}, status: 401
@@ -12,13 +12,14 @@ class EventsController < ApplicationController
   end
 
   api :POST, '/events'
-  param :event, Hash, :desc => "Wrapper key", required: true do
-    param :title, String, :desc => "Title of the event", :required => true
-    param :description, String, :desc => "Description of the event", :required => true
-    param :start_time, String, :desc => "When the event starts - date and time", :required => true
-    param :end_time, String, :desc => "When the event ends - date and time", :required => true
-    param :icon, String, :desc => "String code representing the icon", :required => true
+  param :event, Hash, desc: "Wrapper key", required: true do
+    param :title, String, desc: "Title of the event", required: true
+    param :description, String, desc: "Description of the event", required: true
+    param :start_time, String, desc: "When the event starts - date and time", required: true
+    param :end_time, String, desc: "When the event ends - date and time", required: true
+    param :icon, String, desc: "String code representing the icon", required: true
   end
+
   def create
     event = Event.new(event_params)
     if event.valid?
@@ -30,6 +31,7 @@ class EventsController < ApplicationController
   end
 
 private
+
   def event_params
     params.require(:event).permit(:title, :description, :start_time, :end_time, :icon)
   end
