4af3766a074e90fe76a4d23c99bb090e6961c204
Zuzana Dostalova <zuzana.dostalova@merck.com>
2016-03-08 13:18:42 +0100

    Apipie fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,6 +24,8 @@ class EventsController < ApplicationController
     param :end_time, String, desc: "When the event ends - date and time", required: true
     param :icon, String, desc: "String code representing the icon", required: true
   end
+  error code: 401, desc: "Wrong authorization token"
+  error code: 400, desc: "Event is not valid"
 
   def create
     event = Event.new(event_params)
@@ -35,6 +37,27 @@ class EventsController < ApplicationController
         render json: {}, status: 201
       else
         render json: {error: event.errors.messages}, status: 400
+      end
+    else
+      render json: {}, status: 401
+    end
+  end
+
+  api!
+  param :token, String, desc: "Authorization token", required: true
+  param :id, Integer, desc: "ID for the event", required: true
+  error code: 401, desc: "Wrong authorization token"
+  error code: 403, desc: "User is not the author of the event"
+
+  def destroy
+    event = Event.find(params[:id])
+
+    if current_user
+      if event.author == current_user
+        event.destroy
+        render json: {}, status: 200
+      else
+        render json: {error: "You are not author of this event!"}, status: 403
       end
     else
       render json: {}, status: 401
