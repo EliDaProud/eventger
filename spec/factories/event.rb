c86daa738d44e3123856b2ec8bef832af1985796
elidaproud <eliska.cejpova@merck.com>
2016-03-14 11:29:24 +0100

    FG location

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     description 'Hobits gathering with trolls singing celtic songs'
     start_time 1.day.from_now
     end_time 4.days.from_now
+    location 'The second dungeon on left'
     icon 'some icon'
   end
 end
